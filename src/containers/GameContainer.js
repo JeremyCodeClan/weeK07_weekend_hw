f5b52a579e7b69faf7d0c5a43eb34cf0fda4da3e
Jeremy Yoo <jyyoo824@gmail.com>
2021-09-04 15:59:13 +0100

    api request success

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+const api_key = process.env.REACT_APP_API_KEY
 
 const GameContainer = () => {
+
+    const [freeGames, SetFreeGames] = useState([])
+
+    useEffect(() => {
+        getFreeGames();
+    }, [])
+
+    const getFreeGames = () => {
+        fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", 
+        {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+            "x-rapidapi-key": api_key,
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            SetFreeGames(data);})
+        .catch(err => console.error(err))
+    }
+
+    console.log(freeGames);
 
     return (
         <>
